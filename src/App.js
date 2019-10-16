1b4a4050efba1e4502fa2199537dad589a699610
gelinas <jlgelinas@gmail.com>
2019-10-16 13:45:30 -0400

    Louis-Gelinas

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>LOUIS IN THE HOUSE BRANCH</h1>
       <h2>
         Deployed with{' '}
         <a
